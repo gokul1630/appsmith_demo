929a5e881cfe5cfc8f2443a002306e7db47cf495
gokul1630 <gokul1630>
2023-03-13 11:56:16 +0000

    used dummy todo data

--- pages/Page1/jsobjects/StudentReport/StudentReport.js
+++ pages/Page1/jsobjects/StudentReport/StudentReport.js
@@ -1,3 +1,21 @@
 export default {
-getData:()=>StudentReportApi.data.map(({x,y})=>({x,y}))
+	
+getData:()=>{
+let comp=0;
+	let notCompleted=0;
+	StudentReportApi.data.map(({completed})=>{
+		if(completed)
+		{ comp+=1}
+		else{
+			notCompleted+=1
+		}
+	})
+return [{x: "completed",y:comp },{x: "notCompleted",y:notCompleted }]
+},
+
+getDropDownData:()=>{
+return	StudentReportApi.data.map(({id,title})=>{return{label:title,value:id}})
 }
+
+}
+
